f478db154d965600e41b245317863cad0dda4524
nina-chargue <nina8chargue@gmail.com>
2025-01-09 00:27:09 -0600

    scrapper_fastapi version 1.0.0

---
+++ scrap.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+from typing import List
+
+def extract_body_content(html_content: str) -> str:
+    soup = BeautifulSoup(html_content, 'html.parser')
+    body_content = soup.body
+    if body_content:
+        return str(body_content)
+    return None
+
+def clean_body_content(body_content: str) -> str:
+    soup = BeautifulSoup(body_content, 'html.parser')
+
+    for script_or_style in soup(["script", "style", "header", "footer", "nav", "aside"]):
+        script_or_style.extract()
+    
+    cleaned_content = soup.get_text(separator="\n")
+    cleaned_content = "\n".join(line.strip() for line in cleaned_content.splitlines() if line.strip())
+    
+    return cleaned_content
+
+def split_dom_content(dom_content: str, max_length: int = 4000) -> List[str]:
+    if len(dom_content) <= max_length:
+        return [dom_content]
+    
+    return [
+        dom_content[i:i + max_length] for i in range(0, len(dom_content), max_length)
+    ]
---
+++ scrapper_api.py
@@ -0,0 +1,45 @@
+from typing import List
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import httpx
+from scrap import extract_body_content, clean_body_content, split_dom_content
+from parse import parse_with_ollama
+
+app = FastAPI()
+
+class WebsiteRequest(BaseModel):
+    url: str
+    parse_description: str
+
+class WebsiteTextInfo(BaseModel):
+    body: List[str]
+
+async def fetch_website_data(url: str) -> dict:
+    try:
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url)
+
+            if response.status_code != 200:
+                raise HTTPException(status_code=404, detail="Website not found")
+            
+            body_content = extract_body_content(response.text)
+            if body_content:
+                body_content = clean_body_content(body_content)
+                body_content = split_dom_content(body_content)
+                data = {"body": body_content}
+            else:
+                data = {"body": ["No body content found"]}
+            
+            return data
+
+    except httpx.HTTPStatusError as e:
+        raise HTTPException(status_code=e.response.status_code, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
+
+@app.post("/extract-info/")
+async def extract_info(request: WebsiteRequest):
+    data = await fetch_website_data(request.url)
+    parsed_data = parse_with_ollama(data['body'], request.parse_description)
+
+    return {"parsed_data": parsed_data}
